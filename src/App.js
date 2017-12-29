4d9cc6ac98052d06e09b73e6fa68b21487134117
Daniel Lennox <lennox.dan@gmail.com>
2017-12-29 21:16:24 +0100

    Misc display fixes.

--- src/App.js
+++ src/App.js
@@ -8,13 +8,17 @@ import ImgList from './Components/ImgList';
 import SearchForm from './Components/SearchForm';
 import cred from './cred.js';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 export default class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			imgs: [],
 			loadingState: true,
-			featureImg: ''
+			featureImg: '',
+      query: 'sun'
 		};
 	}
 
@@ -28,16 +32,34 @@ export default class App extends Component {
     });
   };
 
+	randomImage = (query = 'sun') => {
+	  console.log('called and query is: ', query);
+    axios
+      .get(
+        `https://api.unsplash.com/photos/random/?query=${query}&orientation=portrait&client_id=${cred.APP_ID}`
+      )
+      .then(data => {
+        console.log('data: ', data);
+        this.setState({
+          featureImg: data.data
+        });
+      })
+      .catch(err => {
+        console.log('Error happened during fetching!', err);
+      });
+  };
+
 	performSearch = (query = 'sun') => {
 		axios
 			.get(
-				`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=${query}&client_id=${cred.APP_ID}`
+				`https://api.unsplash.com/search/photos/?page=1&per_page=20&query=${query}&client_id=${cred.APP_ID}`
 			)
 			.then(data => {
 				this.setState({
 					imgs: data.data.results,
 					loadingState: false,
-					featureImg: data.data.results[0]
+					featureImg: data.data.results[0],
+          query: query,
 				});
 			})
 			.catch(err => {
@@ -47,23 +69,26 @@ export default class App extends Component {
 
 	render() {
 		return (
-			<div>
-				<div className="main-header">
-					<div className="inner">
-						<h1 className="main-title">GFH Unsplash Testing</h1>
-						<SearchForm onSearch={this.performSearch} />
-					</div>
-				</div>
-        <div className="feature-area">
-          { (this.state.loadingState) ? null : <FeatureImg img={this.state.featureImg}/> }
-          <img className="content-demo" src={'./content-demo.png'} alt="content demo" />
+      <MuiThemeProvider>
+        <div>
+          <div className="main-header">
+            <div className="inner">
+              <h1 className="main-title">GFH Unsplash Testing</h1>
+              <RaisedButton className="random-image-button" label="Load random image" primary={true} onClick={() => this.randomImage(this.state.query)} />
+              <SearchForm onSearch={this.performSearch} />
+            </div>
+          </div>
+          <div className="feature-area">
+            { (this.state.loadingState) ? null : <FeatureImg img={this.state.featureImg}/> }
+            <img className="content-demo" src={'./content-demo.png'} alt="content demo" />
+          </div>
+          <div className="main-content">
+            {this.state.loadingState
+              ? <p>Loading</p>
+              : <ImgList images={this.state.imgs} select={this.selectImg} />}
+          </div>
         </div>
-				<div className="main-content">
-					{this.state.loadingState
-						? <p>Loading</p>
-						: <ImgList images={this.state.imgs} select={this.selectImg} />}
-				</div>
-			</div>
+      </MuiThemeProvider>
 		);
 	}
 }
